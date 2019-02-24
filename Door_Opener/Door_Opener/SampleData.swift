067513a1561b269e213cf525dd284c779cccf977
Ziheng Yan <zihengyan@ucsb.edu>
2019-02-23 21:44:44 -0800

    implement friend adding function and UI

--- Door_Opener/Door_Opener/SampleData.swift
+++ Door_Opener/Door_Opener/SampleData.swift
@@ -12,9 +12,9 @@ final class SampleData {
     
     static func generatePersonData() -> [Person] {
         return [
-            Person(UID: "UID1",name: "Billy",gender: "Male",userName: "Billy2000",roomReferenceNumber: "CATA5230", authorizedPeople: AuthorizedPeople(UID: "UID1", authorized: true, times: 5, weekday: 3, startTime: 930, endTime: 1530)),
+            Person(UID: "UID1", name: "NAME1", gender: "Male", userName: "UserName1", roomReferenceNumber: "CATA5230"),
             
-            Person(UID: "UID2",name: "name2",gender: "Female",userName: "username2",roomReferenceNumber: "room2", authorizedPeople: AuthorizedPeople(UID: "UID2", authorized: false, times: 0, weekday: 0, startTime: 0, endTime: 0))
+            Person(UID: "UID@", name: "NAME2", gender: "Female", userName: "UserName2", roomReferenceNumber: "CATA8211")
         ]
     }
 }
