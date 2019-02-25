6446fa4dc44e96454bd17a3f30a2b968ea0534d3
Ziheng Yan <zihengyan@ucsb.edu>
2019-02-24 16:11:10 -0800

    modified login view and implement friend adding functions

--- Door_Opener/Door_Opener/Controllers/FriendsViewController.swift
+++ Door_Opener/Door_Opener/Controllers/FriendsViewController.swift
@@ -7,16 +7,30 @@
 //
 
 import UIKit
+import Firebase
 
 class FriendsViewController: UITableViewController {
-    var friends = SampleData.generatePersonData()
     
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        // Do any additional setup after loading the view.
-//    }
-//
+    let ref = Database.database().reference(withPath: "Person")
+    
+    //var friends = SampleData.generatePersonData()
+    var friends: [Person] = []
+    var user: Person!
+    //
+    // TODO: check the purpose of the following overriding function
+    //
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        user = Person(UID: "UID1", name: "NAME1", gender: "Male", userName: "UserName1", roomReferenceNumber: "CATA5230")
+    }
+
 
     /*
     // MARK: - Navigation
@@ -34,9 +48,27 @@ extension FriendsViewController {
     
     @IBAction func cancelToFriendsViewController(_ segue: UIStoryboardSegue) {}
     @IBAction func saveFriendDetail(_ segue: UIStoryboardSegue) {}
-}
+    @IBAction func addButtonDidTouch(_ sender: AnyObject) {
+        let alert = UIAlertController(title: "Friend",
+                                      message: "Add a Friend",
+                                      preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { _ in
+            let textField = alert.textFields![0]
+            
+            let friend = Person(UID: "UIDTest", name: "NameTest", gender: "Male", userName: textField.text!, roomReferenceNumber: "Room")
+            
+            self.friends.append(friend)
+            self.tableView.reloadData()
+        
+        }
+        
+        alert.addTextField()
+        
+        alert.addAction(saveAction)
+        present(alert, animated: true, completion: nil)
+    }
 
-extension FriendsViewController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return friends.count
     }
@@ -45,6 +77,21 @@ extension FriendsViewController {
         
         let friend = friends[indexPath.row]
         cell.person = friend
+        //
+        // TODO: change the following statement so that authorization status can be changed
+        //
+        //toggleCellCheckbox(cell, true: friend.authorizedPeople)
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            friends.remove(at: indexPath.row)
+            tableView.reloadData()
+        }
     }
 }
