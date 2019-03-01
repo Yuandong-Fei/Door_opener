a66c003cf9476f9ada36e0a147821aaca364b3b7
yang634 <46575984+yang634@users.noreply.github.com>
2019-03-01 09:41:22 -0800

    used funtions in the header files

--- arduino_code_door_opener/arduino_code_door_opener.ino
+++ arduino_code_door_opener/arduino_code_door_opener.ino
@@ -1,6 +1,8 @@
 #include <Servo.h>
 #include <string.h>
-#include <door_opener_header.h>
+#include "door_opener_header.h"
+#include "open.h"
+#include "close.h"
 void setup() {
   Serial.begin(9600);
 }
@@ -10,21 +12,10 @@ void loop() {
     validation = Firebase.getString("User", "announcement");
     delay(1000);
     if (validation == "Authorized") { //see if the person's authorized, then utilize the servo, and turn on the LED
-      servo.attach(servoPin);
-      servo.write(0)
-      delay(500);
-      servo.detach();
-      digitalWrite(LCDpin, HIGH);//notify user it's on
-      Firebase.setString("servo", "true");
-      servo1 = Firebase.getString("servo")
+              door.opener();
               delay(30000);
       if (servo1 == "true" && MagneticSwitch) {
-        delay(1000);
-        servo.attach(servoPin);
-        servo.write(180);
-        delay(500);
-        servo.detach();
-        delay(3000);
+              door.close();
       }
 
     }
